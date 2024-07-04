fbd4e406fc495f4d726447202d7e6effc25b44da
UlisesCasero <107223218+UlisesCasero@users.noreply.github.com>
2024-07-03 22:07:15 -0300

    modified:   Frontend/src/app/app.component.scss 	modified:   Frontend/src/app/home-cliente/home-cliente.component.html 	modified:   Frontend/src/app/home-cliente/home-cliente.component.ts 	modified:   Frontend/src/app/shared/aside/aside.component.html 	modified:   Frontend/src/services/carrito.service.ts

--- Frontend/src/app/home-cliente/home-cliente.component.ts
+++ Frontend/src/app/home-cliente/home-cliente.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiciosService } from 'src/services/servicios.service';
 import { CarritoService } from 'src/services/carrito.service';
-import { Producto } from 'src/models/producto';
 import { ProductoService } from 'src/services/producto.service';
 import { ProductoCarrito } from 'src/models/productoCarrito';
+import { Subscription, take } from 'rxjs';
 
 @Component({
   selector: 'app-home-cliente',
@@ -11,33 +11,64 @@ import { ProductoCarrito } from 'src/models/productoCarrito';
   styleUrls: ['./home-cliente.component.scss']
 })
 export class HomeClienteComponent {
-  productos: any[] = [];
-  notificationMessage: string = '';
-  productosFiltrados: any[] = [];
+  productos: ProductoCarrito[] = [];
+  productosFiltrados: ProductoCarrito[] = [];
   terminoBusqueda: string = '';
+  notificationMessage: string = '';
+  carritoSub!: Subscription;
+  
   constructor(private serviciosService: ServiciosService, private carritoSvc: CarritoService, private productosSvc: ProductoService) { }
 
   ngOnInit(): void {
     this.obtenerProductos();
+    this.carritoSub = this.carritoSvc.miCarrito$.subscribe((data) => {
+      this.actualizarCantidades(data);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.carritoSub) {
+      this.carritoSub.unsubscribe();
+    }
   }
 
   obtenerProductos() {
-    if (this.isUser()) {
-      this.productosSvc.getProductosCarrito()
-        .subscribe(
-          (data) => {
-            this.productos = data;
-            this.productosFiltrados = this.productos;
-            console.log('productos:', this.productos);
-          },
-          (error) => {
-            console.error('Error al obtener productos:', error);
-          }
-        );
-    } else {
-      console.log('El usuario no tiene permisos para obtener productos.');
-    }
+    this.productosSvc.getProductosCarrito().subscribe(
+      (data) => {
+        this.productos = data;
+        this.productosFiltrados = this.productos;
+        this.carritoSvc.miCarrito$.pipe(take(1)).subscribe(carrito => {
+          this.actualizarCantidades(carrito);
+        });
+      },
+      (error) => {
+        console.error('Error al obtener productos:', error);
+      }
+    );
   }
+
+
+  actualizarCantidades(carrito: ProductoCarrito[]) {
+    this.productos.forEach((producto) => {
+      const productoEnCarrito = carrito.find(p => p.id === producto.id);
+      if (productoEnCarrito) {
+        producto.cantidad = productoEnCarrito.cantidad;
+      } else {
+        producto.cantidad = 0;
+      }
+    });
+    this.productosFiltrados = [...this.productos];
+  }
+
+  addCarrito(producto: ProductoCarrito) {
+    console.log('PRODUCTO: ', producto);
+    this.notificationMessage = 'Agregado!';
+    setTimeout(() => {
+      this.notificationMessage = '';
+    }, 500);
+    this.carritoSvc.addProducto(producto);
+  }
+  
   serverBaseUrl = 'http://localhost:3000/uploads/';
 
   getImageUrl(imageName: string | null | ArrayBuffer): string {
@@ -63,13 +94,12 @@ export class HomeClienteComponent {
     return role === 'PANADERO';
   }
 
-  addCarrito(producto: ProductoCarrito) {
-    console.log('PRODUCTO: ', producto);
-    this.notificationMessage = 'Agregado!';
-    setTimeout(() => {
-      this.notificationMessage = '';
-    }, 500);
-    return this.carritoSvc.addProducto(producto);
+  actualizarCantidad(id: number, operacion: string) {
+    this.carritoSvc.actualizarCantidad(id, operacion);
+  }
+
+  borrarProd(id: number) {
+    this.carritoSvc.eliminarProd(id);
   }
 
   filtrarProductos(): void {
